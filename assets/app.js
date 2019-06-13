704eda01d9210db69087ad2c205484d2fb554967
CodesByMo <morganspencer162@gmail.com>
2019-06-12 21:31:49 -0700

    working but not styled

--- assets/app.js
+++ assets/app.js
@@ -1,81 +1,216 @@
-// app that displays animal gifs on input
-// empty array for storing inputs/gifs
+$(document).ready(function () {
+    // app that displays animal gifs on input
+    // empty array for storing inputs/gifs
 
-// grab search input and store in searchTerms array
-var topics = ["test1", "test2", "test3", "test5", "test6", "test7", "test8", "test9", "test10"]
+    // grab search input and store in searchTerms array
+    var topics = ["test1", "test2"];
+
+    function renderButtons() {
+        $("#gif-dump").empty();
+        $("#button-div").empty();
+        for (var i = 0; i < topics.length; i++) {
+            var newBtn = $("<button>");
+            newBtn.text(topics[i]);
+            newBtn.attr("data-type", topics[i]);
+            newBtn.addClass("testBtn btn btn-dark");
+            $("#button-div").append(newBtn);
+
+        }
+    }
+    renderButtons();
+
+    // $('#submitButton').on('click', function () {
+    //     var userType = $('#userText').val();
+    //     topics.push(userType);
+    //     console.log(topics);
+
+    // });
+    // console.log(topics);
 
 
-$('#add-gif').on('click', function () {
-    $('#image-input').each(function () {
-        topics.push($(this).val());
-        console.log(topics);
+    // function addTo() {
+    //     topics.push($("#image-input").val());
+    // }
+
+    // makes buttons for each item in array
+
+    /////////////////////////new/////////////////////////////////////////////
+    $("#submitButton").on('click', function () {
+        event.preventDefault();
+
+        var userType = $('#userText').val();
+        // var newBtn = $("<button>");
+        // newBtn.text(userType);
+        // newBtn.attr("data-type", userType);
+        // $("#button-div").append(newBtn);
+        // $("#gif-dump").empty();
+        topics.push(userType);
+        // console.log(topics);
+        renderButtons();
+
+
     });
-});
+    console.log(topics);
+
+    $(document).on("click", ".testBtn", function () {
+        $("#gif-dump").empty();
+
+        var dataBtn = $(this).attr("data-type");
+        // ajax for calling to giphy api
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dataBtn + "&api_key=vhCJlVOgblzc6OCgihCPSyI30TEXkdqj";
+
+        // need another function to display images?
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            // After data comes back from the request
+            .then(function (response) {
+                console.log(response);
+
+                // console.log(response.data);
+                // storing the data from the AJAX request in the topics variable
+                // var searchImage = response.data.url;
+
+
+                // Looping through each result item
+                for (var i = 0; i < 10; i++) {
+                    var div = $("<div>");
+                    div.addClass("col-8");
+
+
+                    // Creating and storing an image tag
+                    var imageUrl = response.data[i].images.original.url;
+                    var imgRating = response.data[i].rating;
+
+                    var searchImage = $("<img>").attr("src", imageUrl);
+                    var p = $("<p>");
+                    p.text(imgRating);
+
+
+                    searchImage.addClass("classImg");
+                    searchImage.attr("data-still", response.data[i].images.original_still.url);
+                    searchImage.attr("data-animate", response.data[i].images.original.url);
+                    searchImage.attr("data-state", "animate");
+                    // Setting the src attribute of the image to a property pulled off the result item
+                    // searchImage.attr("src", imageUrl);
+                    console.log(imageUrl);
+                    // gifDiv.append(searchImage);
+                    div.append(searchImage, p);
+                    // Prependng to the HTML page in the "#gif-dump" div
+                    $("#gif-dump").append(div);
+
+                }
+                // $(".newBtn").append(searchImage);
+
+            });
+
+    });
 
 
 
-// function addTo() {
-//     topics.push($("#image-input").val());
-// }
-
-// makes buttons for each item in array
-for (var i = 0; i < topics.length; i++) {
-    var newBtn = $("<button>" + topics[i] + "</button>");
-
-    newBtn.text = topics[i];
-    newBtn.attr("class", "newBtn");
-    $("#gif-dump").append(newBtn);
-}
+    // add pausing/still
 
 
-// delete text in buttons-view before adding new text
-// $("#buttons-view").empty();
-
-
-$(".newBtn").on("click", function () {
-    // ajax for calling to giphy api
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=vhCJlVOgblzc6OCgihCPSyI30TEXkdqj";
-
-    // need another function to display images?
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
+    $("body").on("click", ".classImg", function () {
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var state = $(this).attr("data-state");
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        // Else set src to the data-still value
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+        console.log(state);
     })
-        // After data comes back from the request
-        .then(function (response) {
 
-            // console.log(response.data);
-            // storing the data from the AJAX request in the topics variable
-            // var searchImage = response.data.url;
+})
 
 
-            // Looping through each result item
-            for (var i = 0; i < topics.length; i++) {
-
-                // Creating and storing a div tag
-                // var gifDiv = $("<div>");
-
-                // Creating a paragraph tag with the result item's rating
-                // var p = $("<p>").text("Rating: " + topics[i].rating);
-
-                // Creating and storing an image tag
-                var imageUrl = (response).data.image_original_url;
-
-                var searchImage = $("<img>").attr("src", imageUrl);
-                // Setting the src attribute of the image to a property pulled off the result item
-                // searchImage.attr("src", imageUrl);
-
-                // gifDiv.append(searchImage);
-
-                // Prependng to the HTML page in the "#gif-dump" div
-                $("#gif-dump").prepend(response);
-
-            }
-            $(".newBtn").append(searchImage);
 
 
-        });
-});
 
-// pause/play gifs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //  $("#add-gif").on("click", function () {
+//         // ajax for calling to giphy api
+//         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=vhCJlVOgblzc6OCgihCPSyI30TEXkdqj";
+
+//         // need another function to display images?
+
+//         $.ajax({
+//             url: queryURL,
+//             method: "GET"
+//         })
+//             // After data comes back from the request
+//             .then(function (response) {
+
+//                 // console.log(response.data);
+//                 // storing the data from the AJAX request in the topics variable
+//                 // var searchImage = response.data.url;
+
+
+//                 // Looping through each result item
+//                 for (var i = 0; i < topics.length; i++) {
+
+//                     // Creating and storing a div tag
+//                     // var gifDiv = $("<div>");
+
+//                     // Creating a paragraph tag with the result item's rating
+//                     // var p = $("<p>").text("Rating: " + topics[i].rating);
+
+//                     // Creating and storing an image tag
+//                     var imageUrl = response.data.image_original_url;
+
+//                     var searchImage = $("<img>").attr("src", imageUrl);
+//                     // Setting the src attribute of the image to a property pulled off the result item
+//                     // searchImage.attr("src", imageUrl);
+
+//                     // gifDiv.append(searchImage);
+
+//                     // Prependng to the HTML page in the "#gif-dump" div
+//                     $("#gif-dump").prepend(response);
+
+//                 }
+//                 $(".newBtn").append(searchImage);
+
+
+//             });
+//     });
